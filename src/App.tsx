64d6532bcedb8b0acb71160b2d47498ea335095a
luisricarte <luis.ricarte.ext@synchro.com.br>
2024-02-27 19:38:28 -0300

    adicionando botão com estados

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import { Button } from './components/Button';
 function App() {
   return (
     <>
-    <h1> Hello World!</h1>
-    <Button text="Botão 1"/>
-    <Button>teste</Button>
-    <Button text="Botão 3"/>
+    <Button />
+    <Button></Button>
+    <Button/>
     <Button />
 
     
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,17 @@
-type ButtonProps = {
-  text?: string
-  children?: string
-}
-export function Button(props: ButtonProps) {
+import { useState } from "react";
+
+export function Button() {
+  const [valor, setValor] = useState(0);
+  //USE STATE É INICIADO COM O VALOR DA VARIÁVEL > NUMBER 0
+  //E A SEGUNDA É A FUNÇÃO CALLBACK PARA ALTERAR A FUNÇÃO DE VALOR! 
+  //SETVALOR SERVER PARA ALTERAR O VALOR, ATUALIZÁ-LO.
+  
+  function incrementa(){
+    console.log(valor)
+    setValor(valor+1);
+  }
   return (
-    <button> {props.text || props.children || "Botão padrão"} </button>
+    <button onClick={incrementa}>{valor}</button>
   )
     
   
