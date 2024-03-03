8e62d3245dbf158e1364b338d1d7947f055b1cd0
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-03-03 11:29:30 -0300

    fix: ajustando tela de criação de sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,9 +1,7 @@
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 import {Button} from '../components/Button';
-import { text } from 'stream/consumers';
 
 export function NewRoom() {
     return (
