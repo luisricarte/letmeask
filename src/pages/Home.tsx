d6671b8db8f61f5ca522c6cb77958335c5097ce5
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-03-03 10:07:39 -0300

    adicionando page-auth - aula 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
+import '../styles/auth.scss';
 
 export function Home() {
     return (
-    <>
+    <div id= "page-auth">
     <aside>
         <img src={illustrationImg} alt="Ilustração principal" />
         <strong> Crie salas de Q&A ao-vivo</strong>
@@ -24,6 +25,6 @@ export function Home() {
             </form>
         </div>
     </main>
-    </>
+    </div>
     )
 }
