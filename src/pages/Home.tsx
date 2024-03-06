68186eaa95b99a35b2c18c523803960a3405bd42
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-03-06 19:18:05 -0300

    adicionando erro: 'sala não existe'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useHistory } from 'react-router-dom';
-import { useContext } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 
 import { AuthContext } from '../contexts/AuthContext';
 
@@ -13,18 +13,35 @@ import {Button} from '../components/Button';
 
 import '../styles/global.scss';
 import '../styles/auth.scss';
-
-
+import { database } from '../services/firebase';
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth()
+    const [roomCode, setRoomCode] = useState('')
 
     async function handleCreateRoom(){
         if(!user) {
             await signInWithGoogle()
         }
         history.push('/rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()){
+            alert('Room does not exists.');
+            return;
+        }
+
+        history.push(`rooms/${roomCode}`)
     }
 
     return (
@@ -42,8 +59,12 @@ export function Home() {
                 Crie sua sala com o Google
             </button>
             <div className="separator"> ou entre em uma sala</div>
-            <form action="">
-                <input type="text" placeholder='Digite o código da sala' />
+            <form action="" onSubmit={handleJoinRoom}>
+                <input 
+                    type="text" 
+                    placeholder='Digite o código da sala'
+                    onChange={event => setRoomCode(event.target.value)}
+                    value={roomCode} />
                 <Button> Entrar na sala </Button>
             </form>
         </div>
