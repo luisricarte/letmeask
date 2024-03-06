a9c94ac142c73e50bef5610191ddef375fdabdf6
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-03-05 23:36:17 -0300

    adicionando auth context > melhorando a qualidade do código

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,24 @@
 import { useHistory } from 'react-router-dom';
 import { useContext } from 'react';
 
-import { AuthContext } from '../App';
+import { AuthContext } from '../contexts/AuthContext';
+
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
+
+import { useAuth } from '../hooks/useAuth';
 import {Button} from '../components/Button';
 
 import '../styles/global.scss';
 import '../styles/auth.scss';
 
 
+
 export function Home() {
     const history = useHistory();
-    const { user, signInWithGoogle } = useContext(AuthContext)
+    const { user, signInWithGoogle } = useAuth()
 
     async function handleCreateRoom(){
         if(!user) {
